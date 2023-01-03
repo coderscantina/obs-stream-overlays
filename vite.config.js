131d709fb64a97581f97edd983955361c999ce87
Michael Wallner <m.wallner@neonblack.at>
2023-01-03 13:27:22 +0100

    🔧 include purgecss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import htmlPurge from 'vite-plugin-purgecss'
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
@@ -13,5 +12,5 @@ export default defineConfig({
     strictPort: true,
     port: 5020,
   },
-  plugins: [vue(), htmlPurge()],
+  plugins: [vue()],
 })
