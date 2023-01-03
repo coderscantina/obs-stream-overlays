e9a3224e4342e3ad076f3039c7b7c258a8b17425
Michael Wallner <m.wallner@neonblack.at>
2023-01-03 13:29:14 +0100

    👔 allow header class to be set

--- src/components/BigTextBox.vue
+++ src/components/BigTextBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="big-text-box">
     <FadeText :text="subheader" class="h1 display-1 pb-1" />
-    <FadeText :text="header" class="h1 display-3" />
+    <FadeText :text="header" class="h1 display-3" :class="headerClass" />
   </div>
 </template>
 
@@ -10,6 +10,7 @@ import FadeText from './FadeText.vue'
 
 const props = defineProps({
   header: String,
+  headerClass: String,
   subheader: String,
 })
 </script>
