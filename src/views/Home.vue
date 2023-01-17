6bc73476da0065e8b7a4a856afc3c57f23195a08
Michael Wallner <m.wallner@neonblack.at>
2023-01-17 14:11:06 +0100

    ✨ add home view to link all other views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="home"></div>
+  <div class="flex flex-col">
+    <router-link class="hover:text-pink-600" to="/intro">Intro</router-link>
+    <router-link class="hover:text-pink-600" to="/afk">AFK</router-link>
+    <router-link class="hover:text-pink-600" to="/secrets">Secrets</router-link>
+    <router-link class="hover:text-pink-600" to="/outro">Outro</router-link>
+  </div>
 </template>
 
 <script setup></script>
